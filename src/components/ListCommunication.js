97a0788ad2076015ec75dfc04621413409b88b08
sunghyeon <chjcmy@gmail.com>
2021-04-05 14:28:19 +0900

    Main page change

---
+++ src/components/ListCommunication.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function ListCommunication(props) {
+  return (
+        <div>
+            <Item.Group>
+                <Item>
+                    <Item.Image size='tiny' src='/images/wireframe/image.png' />
+                    <Item.Content>
+                        <Item.Header as='a'>Header</Item.Header>
+                        <Item.Meta>Description</Item.Meta>
+                        <Item.Description>
+                            <Image src='/images/wireframe/short-paragraph.png' />
+                        </Item.Description>
+                        <Item.Extra>Additional Details</Item.Extra>
+                    </Item.Content>
+                </Item>
+
+            </Item.Group>
+        </div>
+  );
+}
+
+export default ListCommunication;
