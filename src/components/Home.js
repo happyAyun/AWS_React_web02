5e63efe1944cb3516f9f87683609cc6d46b72562
eumdengs <eumdengs@gmail.com>
2021-04-06 16:43:59 +0900

    api 준비중

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link, Route} from 'react-router-dom';
 import Main from './Main';
+import Search from './Search';
 import Books from './WeBook/Books';
 import ListCommunication from './Communication/ListCommunication';
 
@@ -12,11 +13,12 @@ const Home = () => {
           <Link to="/main">홈</Link>
            <Link to="/Books">우리들의 도서관</Link>
               <Link to="/ListCommunication">커뮤니케이션</Link>
-            <Link to="/">도서찾기</Link>
+            <Link to="/search">도서찾기</Link>
           </div>
           <Route path="/main" component={Main} exact />
           <Route path="/Books" component={Books} />
           <Route path="/ListCommunication" component={ListCommunication} />
+          <Route path="/search" component={Search} />
       </div>
   );
 };
