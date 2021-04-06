e542920a80b7f4a7c705844cc3d47b21a9d1d0d0
eumdengs <eumdengs@gmail.com>
2021-04-06 15:06:54 +0900

    로그인 모달 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,43 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import LoginModal from './modals/LoginModal';
 import SignupModal from './modals/SignupModal';
 import '../App.css';
+import {Button, Nav} from 'react-bootstrap';
 
-const Header = () => {
+const Header = (props) => {
+  // 로그인 전 우측헤더
   const [loginModalOn, setLoginModalOn] = useState(false);
   const [signupModalOn, setSignupModalOn] = useState(false);
 
   return (
-      <div className="LoginModal">
-          <LoginModal
-              show={loginModalOn}
-              onHide={() => setLoginModalOn(false)}
-          />
-          <SignupModal
-              show={signupModalOn}
-              onHide={() => setSignupModalOn(false)}
-          />
-
-                          <Nav className="ml-auto">
-                              <Nav.Link>
-                                  <Button
-                                      variant="primary"
-                                      onClick={() => setLoginModalOn(true)}
-                                  >
-                                      로그인
-                                  </Button>
-                              </Nav.Link>
-                              <Nav.Link>
-                                  <Button
-                                      variant="primary"
-                                      onClick={() => setSignupModalOn(true)}
-                                  >
-                                      회원가입
-                                  </Button>
-                              </Nav.Link>
-                          </Nav>
-
-    </div>
+        <div className="LoginModal">
+            <LoginModal
+                show={loginModalOn}
+                onHide={() => setLoginModalOn(false)}
+            />
+            <SignupModal
+                show={signupModalOn}
+                onHide={() => setSignupModalOn(false)}
+            />
+            <Nav className="ml-auto">
+                <Nav.Link>
+                    <Button
+                        variant="primary"
+                        onClick={() => setLoginModalOn(true)}
+                    >
+                        로그인
+                    </Button>
+                </Nav.Link>
+                <Nav.Link>
+                    <Button
+                        variant="primary"
+                        onClick={() => setSignupModalOn(true)}
+                    >
+                        회원가입
+                    </Button>
+                </Nav.Link>
+            </Nav>
+        </div>
   );
 };
 
--- src/components/modals/LoginModal.js
+++ src/components/modals/LoginModal.js
@@ -5,68 +5,93 @@ import { Link } from 'react-router-dom';
 function LoginModal(props) {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [idError, setIdError] = useState('');
+  const [passError, setPassError] = useState('');
 
-  // 유효성 검사
-  const validate = (id, password) =>{
-    return (id && id.length > 0) && (password && password.length > 0);
+  const idCheck = (e) => {
+    if (id.length >= 3) {
+      return true;
+    }
+    setIdError('3자리 이상 입력해주세요');
+    return false;
+  };
+
+  const passCheck = (e) => {
+    if (password.length >= 8) {
+      return true;
+    }
+    setPassError('8자리 이상 입력해주세요');
+    return false;
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    idCheck();
+    passCheck();
+    if (idCheck() && passCheck()) {
+      alert('wow');
+      // props.switchKey();
+    }
   };
 
   return (
-      <div className="container">
-          <div className="Login">
-              <Modal
-                  show={props.show}
-                  onHide={props.onHide}
-                  size="lg"
-                  aria-labelledby="contained-modal-title-vcenter"
-                  centered
-              >
-                  <Container>
-                      <Modal.Header closeButton>
-                          <Modal.Title id="contained-modal-title-vcenter">로그인</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                          <Form>
-                              <Form.Group>
-                                  <Form.Label>아이디</Form.Label>
-                                  <FormControl
-                                      autoFocus
-                                      value={id}
-                                      onChange={e => setId(e.target.value)}
-                                  />
-                              </Form.Group>
+        <div className="container">
+            <div className="Login">
+                <Modal
+                    show={props.show}
+                    onHide={props.onHide}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Container>
+                        <Modal.Header closeButton>
+                            <Modal.Title id="contained-modal-title-vcenter">로그인</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>아이디</Form.Label>
+                                    <FormControl
+                                        autoFocus
+                                        value={id}
+                                        onChange={e => setId(e.target.value)}
+                                    />
+                                    <div style={{ color: 'red', fontSize: '12px' }}>{idError}</div>
+                                </Form.Group>
 
-                              <Form.Group>
-                                  <Form.Label>비밀번호</Form.Label>
-                                  <FormControl
-                                      value={password}
-                                      onChange={e => setPassword(e.target.value)}
-                                      type="password"
-                                  />
-                              </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>비밀번호</Form.Label>
+                                    <FormControl
+                                        value={password}
+                                        onChange={e => setPassword(e.target.value)}
+                                        type="password"
+                                    />
+                                    <div style={{ color: 'red', fontSize: '12px' }}>{passError}</div>
+                                </Form.Group>
 
-                              <Button
-                                  block
-                                  bsSize="large"
-                                  variant="info"
-                                  disabled={!validate(id, password)}
-                                  type="submit"
-                              >
-                                  Login
-                              </Button>
-                              <Button
-                                  block
-                                  bsSize="large"
-                                  type="submit"
-                              >
-                                  <Link to="/Signup"><span className="signup_text">Signup</span></Link>
-                              </Button>
-                          </Form>
-                      </Modal.Body>
-                  </Container>
-              </Modal>
-          </div>
-      </div>
+                                <Button
+                                    block
+                                    bsSize="large"
+                                    variant="info"
+                                    type="submit"
+                                    onClick={onSubmitHandler}
+                                >
+                                    Login
+                                </Button>
+                                <Button
+                                    block
+                                    bsSize="large"
+                                    type="submit"
+                                >
+                                    <Link to="/Signup"><span className="signup_text">Signup</span></Link>
+                                </Button>
+                            </Form>
+                        </Modal.Body>
+                    </Container>
+                </Modal>
+            </div>
+        </div>
   );
 }
 
