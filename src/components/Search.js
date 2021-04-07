27032209b12ea2c29f616b1be59707e6405aa41a
SangRakKun <leesangrak0307@gmail.com>
2021-04-07 16:33:22 +0900

    심아윤 개이뻐

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,69 +1,70 @@
-import React, {useState, useEffect} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import Movies from '../components/Movies';
+import './Home.css';
 
-const Search = () => {
-  const [term, setTerm] = useState('programming');
-  const [results, setResults] = useState([]);
-
-  const headers = {
-    'Content-Type': 'text/plain'
-  };
-    
-  useEffect((url, config) => {
-    const search = async () => {
-      const data = await axios.get('https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&size=10&target=title', {
-        headers: {
-          Authorization: 'KakaoAK b2097d36d494cbe565a431b90c53c9c6'
-        },
-        params: {
-          query: '어린왕자'
-        }
-      }).then(response => {
-        console.log(response);
-      });
-      console.log(data);
+class Home extends Component {
+    state = {
+      isLoading: true,
+      documents: [],
     };
-    search();
-  });
 
-  const renderedResults = results.map((result) => {
-    return (
-            <div className="item" key={result.pageid}>
-                <div className="right floated content">
-                    <a
-                        className="ui button"
-                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
-                    >
-                        Go
-                    </a>
-                </div>
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} >
-              </span>
-                </div>
-            </div>
-    );
-  });
+    getMovies = async () => {
+      const {
+        data: { documents },
+      } = await axios.get(
+        'https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&size=10&target=title',
+        {
+          headers: {
+            Authorization: 'KakaoAK b2097d36d494cbe565a431b90c53c9c6',
+          },
+          params: {
+            query: '어린왕자',
+          },
+        }
+      );
+      this.setState({ documents, isLoading: false });
+    };
 
-  return (
-        <div>
-            <div className="ui form">
-                <div className="field">
+    componentDidMount() {
+      this.getMovies();
+    }
+
+    render() {
+      const { isLoading, documents } = this.state;
+      return (
+            <div>
+                <div className="ui form">
                     <label>Enter Search Term</label>
                     <input
-                        value={term}
-                        onChange={e => setTerm(e.target.value)}
                         className="input" />
                 </div>
-            </div>
-            <div className="ui called list">
-                {renderedResults}
-            </div>
-        </div>
-  );
-};
+                <section className="container">
+                    {isLoading ? (
+                        <div className="loader">
+                            <span className="loader__text">loading...</span>
+                        </div>
 
-export default Search;
+                    ) : (
+
+                        <div className="movies">
+                            {documents.map((movie) => {
+                              // console.log(movie);
+                              return (
+                                    <Movies
+                                        id={movie.isbn}
+                                        title={movie.title}
+                                        summary={movie.contents}
+                                        poster={movie.thumbnail}
+                                    />
+                              );
+                            })}
+                        </div>
+                    )}
+                </section>
+            </div>
+      );
+    }
+}
+
+export default Home;
