04d6879c9923c08a0bf5f1a4112934590c942a94
Ayun Sim <shim52111@gmail.com>
2021-04-09 09:50:19 +0900

    수정

--- src/components/Communication/ListCommunication.js
+++ src/components/Communication/ListCommunication.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import { Image, Item } from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Item, ItemGroup} from 'semantic-ui-react';
 import SearchBar from '../SearchBar.js';
+import {Nav} from 'react-bootstrap';
+import CreateCommunication from './CreateCommunication';
+import {Link} from 'react-router-dom';
+
 function ListCommunication(props) {
+  const [search, setSearch] = useState('');
+
   return (
         <div>
-            <SearchBar />
-            <Item.Group>
-                <Item>
+
+            <Nav>
+                <SearchBar />
+                <Link to={'/CreateCommunication'}><button>생성</button></Link>
+            </Nav>
+
+            <table>
+                <ul>
                     <Item.Image size='tiny' src='/images/wireframe/image.png' />
                     <Item.Content>
                         <Item.Header as='a'>Header</Item.Header>
                         <Item.Meta>Description</Item.Meta>
                         <Item.Description>
-                            <Image src='/images/wireframe/short-paragraph.png' />
                         </Item.Description>
                         <Item.Extra>Additional Details</Item.Extra>
                     </Item.Content>
-                </Item>
+                    ------------
+                    <Item.Content>
+                        <Item.Header as='a'>Header</Item.Header>
+                        <Item.Meta>Description</Item.Meta>
+                        <Item.Description>
+                        </Item.Description>
+                        <Item.Extra>Additional Details</Item.Extra>
+                    </Item.Content>
+                </ul>
 
-            </Item.Group>
+            </table>
         </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,26 +5,26 @@ import Search from './Search';
 import Books from './WeBook/Books';
 import ListCommunication from './Communication/ListCommunication';
 import MyPage from './MyPage/MyPage';
-import MemoDetail from './Memo/MemoDetail';
+import CreateCommunication from './Communication/CreateCommunication';
 
 const Home = () => {
   return (
 
-      <div className="main-bar">
-          <div className="menu-Bar">
-          <Link to="/">홈</Link>
-           <Link to="/Books">우리들의 도서관</Link>
-              <Link to="/ListCommunication">커뮤니케이션</Link>
-            <Link to="/search">도서찾기</Link>
-              <Link to="/mypage">마이페이지</Link>
-          </div>
-          <Route path="/" component={Main} exact />
-          <Route path="/Books" component={Books} />
-          <Route path="/ListCommunication" component={ListCommunication} />
-          <Route path="/search" component={Search} />
-          <Route path="/mypage" component={MyPage}/>
-          <Route path="/mypage/ListMyQnA/MemoDetail" component={MemoDetail}/>
-      </div>
+        <div className="main-bar">
+            <div className="menu-Bar">
+                <Link to="/">홈</Link>
+                <Link to="/Books">우리들의 도서관</Link>
+                <Link to="/ListCommunication">커뮤니케이션</Link>
+                <Link to="/search">도서찾기</Link>
+                <Link to="/mypage">마이페이지</Link>
+            </div>
+            <Route path="/" component={Main} exact />
+            <Route path="/Books" component={Books} />
+            <Route path="/ListCommunication" component={ListCommunication} />
+            <Route path="/search" component={Search} />
+            <Route path="/mypage" component={MyPage}/>
+            <Route path="/CreateCommunication" component={CreateCommunication}/>
+        </div>
   );
 };
 
