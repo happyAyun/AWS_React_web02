41aed039ae337effedf93f78c60c7ba74b85723b
SangRakKun <leesangrak0307@gmail.com>
2021-04-09 17:46:32 +0900

    communication_v2

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import Books from './WeBook/Books';
 import ListCommunication from './Communication/ListCommunication';
 import MyPage from './MyPage/MyPage';
 import CreateCommunication from './Communication/CreateCommunication';
+import ViewCommunication from './Communication/ViewCommunication';
 
 const Home = () => {
   return (
@@ -24,6 +25,7 @@ const Home = () => {
           <Route path="/search" component={Search} />
           <Route path="/mypage" component={MyPage}/>
           <Route path="/CreateCommunication" component={CreateCommunication}/>
+          <Route exact path='/ViewCommunication/:no' component={ViewCommunication} />
       </div>
   );
 };
