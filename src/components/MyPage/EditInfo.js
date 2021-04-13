1b0287326efec99d6dcdb1bb466e2fb9415165a5
eumdengs <>
2021-04-13 15:32:47 +0900

    ing

--- src/components/MyPage/EditInfo.js
+++ src/components/MyPage/EditInfo.js
@@ -4,7 +4,15 @@ import './EditInfo.css';
 function EditProfile(props) {
   return (
         <div>
-
+            <div className="mainBox">
+                <div>
+                    내 정보 관리
+                </div>
+            </div>
+            <hr />
+            <div className="mainBox">
+                <div>프로필</div>
+            </div>
         </div>
 
   );
