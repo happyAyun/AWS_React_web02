b78e1e81ae10278b6e5a25c486ec233b6b7bea67
Ayun Sim <shim52111@gmail.com>
2021-04-20 18:53:37 +0900

    성현님~ ㅎㅎ..ㅎ

--- src/components/MyPage_Route/ListCourse.js
+++ src/components/MyPage_Route/ListCourse.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Card, Icon, Image } from 'semantic-ui-react';
+import axios from 'axios';
 
 function ListCourse(props) {
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    const take = async () => {
+      const {data} = await axios.get('http://localhost:8000/api/book/signbook/', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`,
+        }
+      });
+      setDataList(data);
+    };
+    take();
+  }, []);
+  
   return (
         <div>
-            <Card>
-                <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>Matthew</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        Matthew is a musician living in Nashville.
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <a>
-                        <Icon name='user' />
-                        22 Friends
-                    </a>
-                </Card.Content>
-            </Card>
+            {console.log(dataList)}
+            {
+                dataList ? dataList.map((item) => {
+                  return (
+                        <Card>
+                            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+                            <Card.Content>
+                                <Card.Header></Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>Joined in 2015</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    Matthew is a musician living in Nashville.
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <a>
+                                    <Icon name='user' />
+                                    22 Friends
+                                </a>
+                            </Card.Content>
+                        </Card>
+                  );
+                }) : ''
+            }
+
         </div>
   );
 }
